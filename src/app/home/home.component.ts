3090601fce64f2a3011f49ce43eea57f7022476d
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-17 17:51:38 +0300

    Products service added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+import { Products } from '../../types';
+import { ProductsService } from './../services/products.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor
+    (private productsService: ProductsService
+    ) { }
 
+  ngOnInit() {
+    this.productsService.getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => { console.log(products.items); });
+  }
 }
