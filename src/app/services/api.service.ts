7e203fb12a8129cab680229b28c8aee48aa36607
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-20 13:06:49 +0300

    post put and delete functions added

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Options } from '../../types';
+import { Options, Product } from '../../types';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,18 @@ export class ApiService {
     private httpClient: HttpClient) { }
 
   get<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
+
+  post<T>(url: string, body: Product, options: Options): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
+
+  put<T>(url: string, body: Product, options: Options): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
+
+  delete<T>(url: string, body: Product, options: Options): Observable<T> {
     return this.httpClient.get<T>(url, options) as Observable<T>;
   }
 }
