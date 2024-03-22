7b9be1ab6567e507d1762c5d8e375a8bafe8321a
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-22 09:37:31 +0300

    inputs outputs added

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { Product } from '../../../types';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
 
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
-  imports: [],
+  imports: [DialogModule, CommonModule, FormsModule, RatingModule],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss'
 })
 export class EditPopupComponent {
+  @Input() display: boolean = false;
+  @Input() header!: string;
+  @Output() confirm = new EventEmitter<Product>();
+  @Output() cancel = new EventEmitter<void>();
+
+  @Input() product: Product = {
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+
+  }
+
+  onConfirm() {
+    this.confirm.emit(this.product);
+  }
+
+  onCancel() {
+    this.display = false;
+  }
 
 }
