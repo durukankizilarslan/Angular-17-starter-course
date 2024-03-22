93d221b0d83056e12cbde681eb20418665cec123
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-22 09:34:37 +0300

    Product add, edit, delete functions added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,6 +20,46 @@ export class HomeComponent {
   totalRecords: number = 0;
   rows: number = 5;
 
+  displayEditPopup: boolean = false;
+  displayAddPopup: boolean = false;
+
+  toggleEditPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+
+  toggleDeletePopup(product: Product) {
+
+  }
+
+  toggleAddPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+
+  selectedProduct: Product = {
+    id: 0,
+    name: "",
+    image: "",
+    price: "",
+    rating: 0,
+
+
+  }
+
+  onConfirmEdit(product: Product) {
+    if (!this.selectedProduct.id) {
+      return;
+    }
+
+    this.editProduct(product, this.selectedProduct.id);
+    this.displayEditPopup = false;
+  }
+  onConfirmAdd(product: Product) {
+    this.addProduct(product);
+    this.displayAddPopup = false;
+  }
+
   onProductOutput(product: Product) {
     console.log(product, "Output");
   };
