0c9cbe32e035ab546b080716af923ab5f895da68
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-22 09:34:55 +0300

    edit and delete product functions added

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -13,9 +13,18 @@ import { FormsModule } from '@angular/forms';
 })
 export class ProductComponent {
   @Input() product!: Product;
-  @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+
+  editProduct() {
+    this.edit.emit(this.product);
+  }
+
+  deleteProduct() {
+    this.delete.emit(this.product);
+  }
 
   ngOnInit() {
-    this.productOutput.emit(this.product);
+
   }
 }
