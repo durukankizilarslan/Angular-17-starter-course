68951ad392ceb645ef551ff347351e119c7fad9c
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-03-25 00:07:24 +0300

    forms added

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -2,18 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../../types';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
-  imports: [DialogModule, CommonModule, FormsModule, RatingModule, ButtonModule],
+  imports: [DialogModule, CommonModule, FormsModule, RatingModule, ButtonModule, ReactiveFormsModule],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss'
 })
 export class EditPopupComponent {
+  constructor(private formBuilder: FormBuilder) { }
   @Input() display: boolean = false;
   @Input() header!: string;
   @Output() confirm = new EventEmitter<Product>();
@@ -30,8 +31,38 @@ export class EditPopupComponent {
 
   }
 
+  specialCharacterValidator(): ValidatorFn {
+    return (control) => {
+      const hasSpecialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(
+        control.value
+      );
+
+      return hasSpecialCharacter ? { hasSpecialCharacter: true } : null;
+    };
+  }
+
+  productForm = this.formBuilder.group(
+    {
+      name: ['', [Validators.required, this.specialCharacterValidator()]],
+      image: [''],
+      price: ['', [Validators.required]],
+      rating: [0],
+    }
+  );
+
+  ngOnChanges() {
+    this.productForm.patchValue(this.product);
+  }
+
   onConfirm() {
-    this.confirm.emit(this.product);
+    const { name, image, price, rating } = this.productForm.value;
+    this.confirm.emit({
+      name: name || '',
+      image: image || '',
+      price: price || '',
+      rating: rating || 0,
+    }
+    );
     this.display = false;
     this.displayChange.emit(this.display);
   }
